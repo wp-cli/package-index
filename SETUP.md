3bb71c0c7f12298ef2a17ca3a6c43396aa38d725
scribu <mail@scribu.net>
2013-09-15 17:19:38 +0300

    install Satis automatically and update setup instructions

--- SETUP.md
+++ SETUP.md
@@ -8,12 +8,4 @@ The following instructions are meant for developers working on the infrastructur
 ./scripts/setup.sh
 ```
 
-2) [Install Satis](https://github.com/composer/satis#usage) somewhere.
-
-3) Build:
-
-```bash
-satis build
-```
-
-The generated file(s) will be in the `web/` directory.
+2) Set up cron to execute `scripts/cron-build.sh`
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# make another clone
 REMOTE_URL=$(git config remote.origin.url)
 git clone . web/
 cd web/
 git remote set-url origin $REMOTE_URL
 git fetch
 git checkout gh-pages
+
+# install Satis
+cd $HOME
+curl -sS https://getcomposer.org/installer | php
+php composer.phar create-project composer/satis --stability=dev --keep-vcs
